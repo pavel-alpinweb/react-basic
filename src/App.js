132f0bb63988b19cb8ddfb1ba2233bfb54b1a49c
Павел Бездорнов <alpinweb@yandex.ru>
2024-01-04 12:08:20 +0600

    create styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, {useState} from "react";
 import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
+import './styles/App.css'
 
 function App() {
     const [value, setValue] = useState('Текст в инпуте');
 
     return (
         <div className="App">
-            <Counter />
+            <div className="post">
+                <div className="post__content">
+                    <strong className="post__title">1. JavaScript</strong>
+                    <div>
+                        JavaScript - язык программирования
+                    </div>
+                </div>
+                <div className="post__btns">
+                    <button className="post__delete">Удалить</button>
+                </div>
+            </div>
             <br/>
-            <ClassCounter />
+            <Counter/>
             <br/>
+            <h1>{value}</h1>
             <input
                 type="text"
                 value={value}
