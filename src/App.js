caa71aaa4b9ff1462dbeef133e62d98d14bf7241
Павел Бездорнов <alpinweb@yandex.ru>
2024-01-04 11:01:18 +0600

    create first component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  return (
-    <div className="App">Hello react!</div>
-  );
+    const [value, setValue] = useState('Текст в инпуте');
+
+    return (
+        <div className="App">
+            <Counter />
+            <br/>
+            <input
+                type="text"
+                value={value}
+                onChange={event => setValue(event.target.value)}
+            />
+        </div>
+    );
 }
 
 export default App;
