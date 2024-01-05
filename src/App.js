47ff0388c0cffce806c7fc95a1a9bcc7046157d4
Павел Бездорнов <alpinweb@yandex.ru>
2024-01-05 09:40:09 +0600

    create post list

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './styles/App.css'
 import Post from "./components/Post";
 
 function App() {
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript', body: 'JavaScript - язык программирования'},
+        {id: 2, title: 'Java', body: 'Java - язык программирования'},
+        {id: 3, title: 'Blueprint', body: 'Blueprint - язык программирования'},
+    ]);
+
     return (
         <div className="App">
-            <Post post={{id: 1, title: 'JavaScript', body: 'JavaScript - язык программирования'}} />
+            {posts.map((post) => <Post post={post}/>)}
         </div>
     );
 }
