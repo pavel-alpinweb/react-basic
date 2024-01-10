812c7d7c24fcfe5a985bb0a5add934842de3435d
Павел Бездорнов <alpinweb@yandex.ru>
2024-01-10 09:52:56 +0600

    animations

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Post from "./Post";
+import {TransitionGroup, CSSTransition} from "react-transition-group";
 
 const PostList = ({posts, title, remove}) => {
     if (!posts.length) {
@@ -11,7 +12,17 @@ const PostList = ({posts, title, remove}) => {
     return (
         <div>
             <h1 style={{textAlign: 'center'}}>{title}</h1>
-            {posts.map((post, index) => <Post remove={remove} number={index + 1} post={post} key={post.id}/>)}
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        className="post"
+                    >
+                        <Post remove={remove} number={index + 1} post={post} />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 };
