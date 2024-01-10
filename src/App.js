62f6a74ce5fcb5ae6d32ced788b63f62a5ebf9bc
Павел Бездорнов <alpinweb@yandex.ru>
2024-01-10 19:14:38 +0600

    create PostService

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {useSortedAndSearchPosts, useSortedPosts} from "./hooks/usePosts";
-import axios from "axios";
+import PostService from "./API/PostService";
 
 function App() {
     const [posts, setPosts] = useState([]);
@@ -17,8 +17,8 @@ function App() {
     const sortedAndSearchPosts = useSortedAndSearchPosts(sortedPosts, filter.query);
 
     async function fetchPosts() {
-        const response = await axios.get('https://jsonplaceholder.org/posts');
-        setPosts(response.data);
+        const posts = await PostService.getAll();
+        setPosts(posts);
     }
 
     const createPost = (newPost) => {
