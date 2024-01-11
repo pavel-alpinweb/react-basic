e70c99f2c154370fbac517cc6fa9760fb4c946fb
Павел Бездорнов <alpinweb@yandex.ru>
2024-01-11 16:41:51 +0600

    comments

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,13 +7,20 @@ import MyLoader from "../components/UI/loader/MyLoader";
 const PostIdPage = () => {
     const params = useParams();
     const [post, setPost] = useState(null);
+    const [comments, setComments] = useState([]);
     const [fetchPostById, isLoading, error] = useFetching(async () => {
         const response = await PostService.getById(params.id);
         setPost(response.data);
     });
 
+    const [fetchComments, isComLoading, errorComments] = useFetching(async () => {
+        const response = await PostService.getPostCommentsById(params.id);
+        setComments(response.data);
+    });
+
     useEffect(() => {
         fetchPostById();
+        fetchComments();
     }, []);
 
     return (
@@ -22,6 +29,20 @@ const PostIdPage = () => {
             {isLoading
                 ? <MyLoader/>
                 : <div>{post?.id}. {post?.title}</div>
+            }
+            <h1>
+                Комментарии
+            </h1>
+            {isLoading
+                ? <MyLoader/>
+                : <div>
+                    {comments.map((comm) =>
+                    <div style={{marginTop: 15}} key={comm.id}>
+                        <h5>{comm.email}</h5>
+                        <div>{comm.body}</div>
+                    </div>
+                    )}
+                </div>
             }
         </div>
     );
