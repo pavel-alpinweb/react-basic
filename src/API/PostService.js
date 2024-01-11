fae7bd947703c7489cb70a85695611d1e65bf4a9
Павел Бездорнов <alpinweb@yandex.ru>
2024-01-11 10:22:01 +0600

    create useFetching

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -2,11 +2,7 @@ import axios from "axios";
 
 export default class PostService {
     static async getAll() {
-        try {
-            const response = await axios.get('https://jsonplaceholder.org/posts');
-            return response.data;
-        } catch (e) {
-            console.log(e);
-        }
+        const response = await axios.get('https://jsonplaceholder.org/posts');
+        return response.data;
     }
 }
--- src/App.js
+++ src/App.js
@@ -8,24 +8,19 @@ import MyButton from "./components/UI/button/MyButton";
 import MyLoader from "./components/UI/loader/MyLoader";
 import {useSortedAndSearchPosts, useSortedPosts} from "./hooks/usePosts";
 import PostService from "./API/PostService";
+import {useFetching} from "./hooks/useFetching";
 
 function App() {
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
-    const [isPostsLoading, setIsPostsLoading] = useState(false);
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const posts = await PostService.getAll();
+        setPosts(posts);
+    });
 
     const sortedPosts = useSortedPosts(posts, filter.sort);
     const sortedAndSearchPosts = useSortedAndSearchPosts(sortedPosts, filter.query);
-
-    async function fetchPosts() {
-        setIsPostsLoading(true);
-        setTimeout(async () => {
-            const posts = await PostService.getAll();
-            setPosts(posts);
-            setIsPostsLoading(false);
-        }, 3000);
-    }
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -49,6 +44,7 @@ function App() {
             </MyModal>
             <PostFilter filter={filter} setFilter={setFilter}/>
             <hr style={{margin: '15px 0'}}/>
+            {postError && <h1>Произошла ошибка ${postError}</h1>}
             {isPostsLoading
                 ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><MyLoader /></div> :
                 <PostList remove={deletePost} posts={sortedAndSearchPosts} title={'Список постов'}/>
