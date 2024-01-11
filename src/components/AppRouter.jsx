4479fe3d2a600d43a7ca69339a13be3f5ccbc476
Павел Бездорнов <alpinweb@yandex.ru>
2024-01-11 17:39:16 +0600

    private and public routes

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import {Redirect, Route, Switch} from "react-router-dom";
-import {routes} from "../router";
+import {privateRoutes, publicRoutes} from "../router";
 
 const AppRouter = () => {
+    const isAuth = false;
     return (
-        <Switch>
-            {routes.map((route) =>
-                <Route
-                    component={route.component}
-                    exact={route.exact}
-                    path={route.path}
-                />
-            )}
-            <Redirect to="/error"/>
-        </Switch>
+        isAuth
+            ?
+            <Switch>
+                {privateRoutes.map((route) =>
+                    <Route
+                        component={route.component}
+                        exact={route.exact}
+                        path={route.path}
+                    />
+                )}
+                <Redirect to="/error"/>
+            </Switch>
+            :
+            <Switch>
+                {publicRoutes.map((route) =>
+                    <Route
+                        component={route.component}
+                        exact={route.exact}
+                        path={route.path}
+                    />
+                )}
+                <Redirect to="/login"/>
+            </Switch>
     );
 };
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import Posts from "../pages/Posts";
 import About from "../pages/About";
 import Error from "../pages/Error";
+import Login from "../pages/Login";
 import PostIdPage from "../pages/PostIdPage";
 
-export const routes = [
+export const privateRoutes = [
     {path: '/', component: Posts, exact: true},
     {path: '/error', component: Error, exact: true},
     {path: '/about', component: About, exact: true},
     {path: '/post/:id', component: PostIdPage, exact: true},
+];
+
+export const publicRoutes = [
+    {path: '/login', component: Login, exact: true}
 ];
